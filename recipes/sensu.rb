de387ffca79adce63bbe60c13fb7d89cf33ed8cc
Jeremy J. Miller <jm@chef.io>
2017-09-28 19:32:00 -0700

    enable all sensu checks for ES
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- recipes/sensu.rb
+++ recipes/sensu.rb
@@ -26,29 +26,30 @@ node['myorg']['sensu']['plugins'].each do |plugin, version|
   end
 end
 
-
-sensu_check 'es_health' do
-  command "check-es-cluster-status.rb -h #{node['ipaddress']}"
-  handlers ["default"]
-  subscribers ["elasticsearch"]
-  interval 30
-  additional(:notification => "Elasticsearch is not running", :occurrences => 5)
+# ES Sensu Plugin Checks
+%w(
+  check-es-cluster-status.rb
+  check-es-circuit-breakers.rb
+  check-es-cluster-health.rb
+  check-es-file-descriptors.rb
+  check-es-heap.rb
+  check-es-indexes.rb
+  check-es-indicies-sizes.rb
+  check-es-node-status.rb
+  check-es-query-count.rb
+  check-es-query-exists.rb
+  check-es-query-ratio.rb
+  check-es-shard-allocation-status.rb
+  handler-es-delete-indices.rb
+  metrics-es-cluster.rb
+  metrics-es-node.rb
+  metrics-es-node-graphite.rb
+).each do |check|
+  sensu_check check do
+    command "#{check} -h #{node['ipaddress']}"
+    handlers ["default"]
+    subscribers ["elasticsearch"]
+    interval 30
+    additional(:notification => "Elasticsearch Sensu: #{check}", :occurrences => 5)
+  end
 end
-
-
-
-#check-es-circuit-breakers.rb
-#check-es-cluster-health.rb
-#bin/check-es-file-descriptors.rb
-#check-es-heap.rb
-#check-es-indexes.rb
-#check-es-indicies-sizes.rb
-#check-es-node-status.rb
-#check-es-query-count.rb
-#check-es-query-exists.rb
-#check-es-query-ratio.rb
-#check-es-shard-allocation-status.rb
-#handler-es-delete-indices.rb
-#metrics-es-cluster.rb
-#metrics-es-node.rb
-#metrics-es-node-graphite.rb
